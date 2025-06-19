2335eacf3cd337e47ef604fd30e43eee00ddb192
Patrik Garten <info@garten.dev>
2025-06-19 03:04:36 +0200

    refactor(devcontainer): simplify port configuration and update Vite server settings
    - Removed port attributes from devcontainer.json for a cleaner configuration.
    - Updated Vite server settings to allow external access and enforce strict port usage.
    - Adjusted development script in package.json to remove unnecessary host specification.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,14 +6,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    host: "0.0.0.0",
-    port: 5173,
-    watch: {
-      usePolling: true,
-      interval: 1000,
-    },
-    hmr: {
-      port: 5173,
-    },
-  },
+    host: true,
+    strictPort: true,
+    // watch: {
+    //   usePolling: true,
+    // },
+  }
 });
